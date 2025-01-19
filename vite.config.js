0bb8a336f4695d1a5e8d691435e4cec9f4ee2350
malorydodge <malorydodge@gmail.com>
2025-01-19 16:42:56 -0500

    add base url back to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default {
+  base: '/greenhouse-gas-emissions/',
   resolve: {
     alias: {
       vue: '@vue/compat',
